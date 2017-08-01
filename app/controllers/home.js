b18d11728fd0dd608ea36e9be2a2c0dda81bf43d
Quach Andy <quachandy99@gmail.com>
2017-08-01 10:44:02 -0400

    fixing require not found error

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -13,3 +13,24 @@ router.get('/', function (req, res, next) {
       articles: articles
     });
 });
+
+
+app.post('/webhook/', function(req, res){
+  let messaging_events = req.body.entry[0].messaging_events;
+  for (let i = 0; i < messaging_events.length; i++) {
+      let event = messaging_events[i];
+      let sender = event.sender.id;
+      if (event.massage && event.massage.text){
+         let text = even.message.text
+        sendText(sender, "text: " + text.substring(0,100));
+    }
+  }
+   res.sendStatus(200);
+});
+
+function sendText(sender, text) {
+  let massageDatabase = {text: text}
+  request({
+    url: ''
+  })
+}
---
+++ app/controllers/webhook.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const router = express.Router();
+
+// module.exports = function(app) {
+//   app.use('/webhook', router);
+// };
+
+// router.get('/', function(req, res) {
+  
+// });
