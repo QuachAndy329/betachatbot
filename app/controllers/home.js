6762207008d65d4a70afa679f925cbe3a7feafed
QuachAndy329 <s3580007@rmit.edu.vn>
2017-08-04 19:29:42 +0700

    Update home.js

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,10 +1,7 @@
+
 var express = require('express'),
   router = express.Router(),
   Article = require('../models/article');
-
-module.exports = function (app) {
-  app.use('/', router);
-};
 
 router.get('/', function (req, res, next) {
   var articles = [new Article(), new Article()];
@@ -13,3 +10,60 @@ router.get('/', function (req, res, next) {
       articles: articles
     });
 });
+
+let token ="EAAKSZA54OsEgBANQLMZBmNTMnhmn1GZBBQSSkoeBhn63cyD8IJUfaWIZBrVefueZBGKTUzXLRnGanrPCdcBCgdgAZClJtndo0GsdZCXWXmeS8JUaEGfkgGQdvO5CIzLxZAqgiN8t0ojvgZAALiCQTkhHrmeAej69DO6ZCxvvZAg8iFn1hdEQFJNKUkJ"
+
+app.get('/', function(req, res){
+    res.send("hi Im a chat bot")  
+})
+
+
+app.get('/webhook/', function(req,res){
+  if (req.query['hub.verify_token'] === "Generator-Express MVC"); {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send("wrong token");
+})
+
+app.listen(app.get('port'), function(){
+        console.log("running: port")   
+           })
+       
+
+
+router.post('/webhook/', function(req, res){
+  let messaging_events = req.body.entry[0].messaging;
+  for (let i = 0; i < messaging_events.length; i++) {
+      let event = messaging_events[i];
+      let sender = event.sender.id;
+      if (event.massage && event.massage.text){
+         let text = event.message.text; 
+        sendText(sender, "text: " + text.substring(0, 100));
+    }
+  }
+   res.sendStatus(200);
+});
+
+
+function sendText(sender, text) {
+  let massageDatabase = {text : text}; 
+  request({
+    url: "https://graph.facebook.com/v2.6/me/messages",
+    qs: {access_token: token},
+    method: "POST",
+    json: {
+      recipient : {id: sender},
+      message :  massageDatabase,         
+  }, function(error, respone, body) { 
+    if (error) {
+        console.log("sending error")
+    } else if (respone.body.error); {
+      console.log("respone body error"); 
+     
+    }  
+  }
+    
+  })
+}
+
+module.exports = router;
