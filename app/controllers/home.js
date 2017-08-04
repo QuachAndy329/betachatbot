e0a70d37cb92d15fcbc4c06d157dfc4cd1b31f78
Quach Andy <quachandy99@gmail.com>
2017-08-04 10:32:58 -0400

    merge fixed bug

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,9 @@
+
 var express = require('express'),
   router = express.Router(),
   Article = require('../models/article');
 
+// the home page
 router.get('/', function (req, res, next) {
   var articles = [new Article(), new Article()];
     res.render('index', {
@@ -10,29 +12,54 @@ router.get('/', function (req, res, next) {
     });
 });
 
-token ="EAAKSZA54OsEgBANQLMZBmNTMnhmn1GZBBQSSkoeBhn63cyD8IJUfaWIZBrVefueZBGKTUzXLRnGanrPCdcBCgdgAZClJtndo0GsdZCXWXmeS8JUaEGfkgGQdvO5CIzLxZAqgiN8t0ojvgZAALiCQTkhHrmeAej69DO6ZCxvvZAg8iFn1hdEQFJNKUkJ"
+let token ="EAAKSZA54OsEgBANQLMZBmNTMnhmn1GZBBQSSkoeBhn63cyD8IJUfaWIZBrVefueZBGKTUzXLRnGanrPCdcBCgdgAZClJtndo0GsdZCXWXmeS8JUaEGfkgGQdvO5CIzLxZAqgiN8t0ojvgZAALiCQTkhHrmeAej69DO6ZCxvvZAg8iFn1hdEQFJNKUkJ"
+
+// the home page again so it confict to the above one 
+// and '' same as '/'
+ 
+
+router.get('/webhook/', function(req,res){
+  if (req.query['hub.verify_token'] === "Generator-Express MVC"); {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send("wrong token");
+})
+
+       
 
 
 router.post('/webhook/', function(req, res){
-  let messaging_events = req.body.entry[0].messaging_events;
+  let messaging_events = req.body.entry[0].messaging;
   for (let i = 0; i < messaging_events.length; i++) {
       let event = messaging_events[i];
       let sender = event.sender.id;
       if (event.massage && event.massage.text){
-         let text = even.message.text
-        sendText(sender, "text: " + text.substring(0,100));
+         let text = event.message.text; 
+        sendText(sender, "text: " + text.substring(0, 100));
     }
   }
    res.sendStatus(200);
 });
 
-// stop for 10 sec ok?
+
 function sendText(sender, text) {
-  let massageDatabase = {text: text}
+  let massageDatabase = {text : text}; 
   request({
     url: "https://graph.facebook.com/v2.6/me/messages",
-    qs: {access_token, token},
-    method: 'POST'
+    qs: {access_token: token},
+    method: "POST",
+    json: {
+      recipient : {id: sender},
+      message :  massageDatabase,         
+  }, function(error, respone, body) { 
+    if (error) {
+        console.log("sending error")
+    } else if (respone.body.error); {
+      console.log("respone body error"); 
+     
+    }  
+  }
+    
   })
 }
 
